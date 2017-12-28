805a48ce84a485b762245c69d83e972343851e40
Yiming Z <yimingzhao3@gmail.com>
2017-12-28 21:35:28 +0800

    Add a simple VGG.
    Really simple.

---
+++ VGG/simple_vgg.py
@@ -0,0 +1,58 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.nn.functional as F
+
+
+class block(nn.Module):
+    def __init__(self):
+        super(block, self).__init__()
+
+        self.conv_1 = nn.Conv2d(
+            in_channels=1,
+            out_channels=3,
+            kernel_size=(3, 3))
+        self.conv_2 = nn.Conv2d(
+            in_channels=3,
+            out_channels=3,
+            kernel_size=(3, 3))
+        self.pool_3 = nn.MaxPool2d(kernel_size=(2, 2))
+
+    def forward(self, x):
+        x = F.relu(self.conv_1(x))
+        x = F.relu(self.conv_2(x))
+        output = self.pool_3(x)
+
+        return output
+
+
+class SimpleVGG(nn.Module):
+    def __init__(self):
+        super(SimpleVGG, self).__init__()
+
+        self.block_1 = block()
+        self.block_2 = block()
+        self.block_2.conv_1 = nn.Conv2d(
+            in_channels=3,
+            out_channels=3,
+            kernel_size=(3, 3)
+        )
+
+        self.fc_1 = nn.Linear(75, 10)
+
+    def forward(self, x):
+        x = self.block_1(x)
+        x = self.block_2(x)
+        x = x.view(x.size(0), -1)
+        output = self.fc_1(x)
+
+        return output
+
+
+if __name__ == '__main__':
+    simplevgg = SimpleVGG()
+    print(simplevgg)
+
+    random_input = Variable(torch.randn(1, 1, 32, 32))
+    output = simplevgg(random_input)
+    print(output)
